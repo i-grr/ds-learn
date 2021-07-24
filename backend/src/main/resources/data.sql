1dd8b34bd1040e92ae1f417e5bdaa5f95e191677
Igor Pereira <igorpereira120@gmail.com>
2021-07-24 19:00:00 -0300

    Offer entity

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,8 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Bootcamp HTML', 'https://www.reliablesoft.net/wp-content/uploads/2019/08/digital-marketing-courses.png', 'https://prnt.sc/1fdw32n');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2021-11-24T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-11-24T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2021-12-24T03:00:00Z', TIMESTAMP WITH TIME ZONE '2022-12-24T03:00:00Z', 1);
